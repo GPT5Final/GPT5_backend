62167da01bc9572cddab079d2bd1b784c04618b5
alstjd974 <alstjd974@nate.com>
2023-04-20 17:05:21 +0900

    이메일 인증

--- src/main/java/gof/gpt5/dao/MemberDao.java
+++ src/main/java/gof/gpt5/dao/MemberDao.java
@@ -3,11 +3,31 @@ package gof.gpt5.dao;
 import org.apache.ibatis.annotations.Mapper;
 import org.springframework.stereotype.Repository;
 
+import gof.gpt5.dto.EmailChkDto;
 import gof.gpt5.dto.MemberDto;
 
 @Mapper
 @Repository
 public interface MemberDao {
+
+	// 이메일 인증
+	int insertEmailKey(EmailChkDto dto);
+	
+	// 이메일 중복확인
+	int emailChk(String email);
+	
+	// 이메일 재전송
+	int updateEmailKey(EmailChkDto dto);
+	
+	// 인증코드 일치 확인
+	EmailChkDto emailKeyChk(EmailChkDto dto);
+	
+	// 인증완료하면 auth 1로 변경
+	int updateMailAuth(EmailChkDto dto);
+	
+	// 이메일 인증완료여부 확인
+	int emailAuthChk(EmailChkDto dto);
+
 	int emailcheck(String email);
 	int addmember(MemberDto dto);
 	MemberDto login(MemberDto dto);

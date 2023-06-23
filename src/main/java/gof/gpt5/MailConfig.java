fbd1f661906cbab09d8530a9dffd898d159032f3
InchaeNa <119583709+dlsco123@users.noreply.github.com>
2023-06-23 23:42:33 +0900

    Update MailConfig.java

--- src/main/java/gof/gpt5/MailConfig.java
+++ src/main/java/gof/gpt5/MailConfig.java
@@ -17,8 +17,8 @@ public class MailConfig {
 		JavaMailSenderImpl javaMailSender = new JavaMailSenderImpl();
 		
 		javaMailSender.setHost("smtp.naver.com"); // 메인 도메인 서버 주소 => 정확히는 smtp 서버 주소
-		javaMailSender.setUsername("dlsco123@naver.com"); // 네이버 아이디
-		javaMailSender.setPassword("gkaqud.1"); // 네이버 비밀번호
+		javaMailSender.setUsername("-.com"); // 네이버 아이디
+		javaMailSender.setPassword("-"); // 네이버 비밀번호
 
 		javaMailSender.setPort(465); // 메일 인증서버 포트
 
